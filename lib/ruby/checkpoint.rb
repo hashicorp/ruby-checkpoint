a4306ed161cdeed3da1dfc02fc0d60ca88b392aa
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-08-31 19:42:53 -0700

    Initial commit

---
+++ lib/ruby/checkpoint.rb
@@ -0,0 +1,7 @@
+require "ruby/checkpoint/version"
+
+module Ruby
+  module Checkpoint
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/checkpoint/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Checkpoint
+    VERSION = "0.0.1"
+  end
+end
